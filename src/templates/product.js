9c370b0707ebb656568042602f4c90ce218c55ca
Jakub Pusiak <jakub.pusiak@gmail.com>
2019-01-09 21:39:53 +0100

    Product page content.

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-const Template = () => (
+const Template = ({
+  data: {
+    markdownRemark: {
+      frontmatter: {
+        title,
+      },
+      html,
+    },
+  },
+}) => (
   <div>
-    Product Here
+    <h1>{title}</h1>
+    <div dangerouslySetInnerHTML={{ __html: html }} />
   </div>
 );
 
 export default Template;
+
+export const query = graphql`
+  query($pathSlug: String!) {
+    markdownRemark(frontmatter: { path: { eq: $pathSlug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
