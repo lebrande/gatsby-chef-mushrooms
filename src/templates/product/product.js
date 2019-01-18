4c72f647694c3fbf7540b85bbd7ca6fa319a5c92
Jakub Pusiak <jakub.pusiak@gmail.com>
2019-01-18 19:28:08 +0100

    Products page logo.

--- src/templates/product/product.js
+++ src/templates/product/product.js
@@ -13,24 +13,34 @@ const Template = ({
       html,
     },
     productImage: {
-      childImageSharp,
+      childImageSharp: productImage,
     },
+    logo: {
+      childImageSharp: logo,
+    }
   },
 }) => (
   <Fragment>
     <div className="product">
+      <div className="product__logo-wrapper">
+        <div className="product__logo">
+          <Img {...logo} />
+        </div>
+      </div>
       <div className="product__images">
         <div
           className="product__image"
           style={{
-            backgroundImage: `url("${childImageSharp.fluid.src}")`,
+            backgroundImage: `url("${productImage.fluid.src}")`,
           }}
         />
       </div>
       <div className="product__content-image">
-        <Img {...childImageSharp} />
+        <Img {...productImage} />
       </div>
       <div className="product__content">
+        <h1>{title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
         <div className="contact__method">
           <span className="icon alt fa-envelope" />
           <h3>
@@ -41,8 +51,6 @@ const Template = ({
           <span className="icon alt fa-phone" />
           <h3>+48 731 044 940</h3>
         </div>
-        <h1>{title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
       </div>
       <div className="product__contact">
         <Footer />
@@ -67,6 +75,13 @@ export const query = graphql`
     productImage: file(relativePath: { eq: $imagePath }) {
       childImageSharp {
         fluid(maxWidth: 1600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    logo: file(relativePath: { eq: "logo/premium-mushrooms-opacity.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 540) {
           ...GatsbyImageSharpFluid
         }
       }
