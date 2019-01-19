e2a74a9fab395326a6287fcb4d8f87d8b1cdb80a
Jakub Pusiak <jakub.pusiak@gmail.com>
2019-01-20 00:02:53 +0100

    H1 tag on home page.

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -4,7 +4,7 @@ const Content = () => (
   <section id="content" className="content">
     <div className="content__container">
       <div className="content__wrapper">
-        <h2 className="content__title">Grzyby premium</h2>
+        <h1 className="content__title">Grzyby premium</h1>
         <p className="content__text">
           W naszej ofercie znajdują się głównie grzyby kuchni orientalnej
           dostępne przez cały rok. Dbamy o najwyższą jakość naszych produktów.
