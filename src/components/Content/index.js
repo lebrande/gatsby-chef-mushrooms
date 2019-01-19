b825aa06ffdded30c23aa328c87a600615b73235
Jakub Pusiak <jakub.pusiak@gmail.com>
2019-01-19 23:02:45 +0100

    Responsive content component.

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -3,15 +3,16 @@ import React from 'react';
 const Content = () => (
   <section id="content" className="content">
     <div className="content__container">
-      <h2 className="content__title">Grzyby premium</h2>
-      <p className="content__text">
-        W naszej ofercie znajdują się głównie grzyby kuchni orientalnej
-        dostępne przez cały rok. Dbamy o najwyższą jakość naszych produktów.
-        Naszą ofertę kierujemy do profesjonalnych kucharzy a także, pasjonatów
-        kuchni. Poniżej przedstawiamy niektóre z naszych produktów. Zapytaj o
-        aktualną ofertę.
-      </p>
-      <p />
+      <div className="content__wrapper">
+        <h2 className="content__title">Grzyby premium</h2>
+        <p className="content__text">
+          W naszej ofercie znajdują się głównie grzyby kuchni orientalnej
+          dostępne przez cały rok. Dbamy o najwyższą jakość naszych produktów.
+          Naszą ofertę kierujemy do profesjonalnych kucharzy a także, pasjonatów
+          kuchni. Poniżej przedstawiamy niektóre z naszych produktów. Zapytaj o
+          aktualną ofertę.
+        </p>
+      </div>
     </div>
   </section>
 );
